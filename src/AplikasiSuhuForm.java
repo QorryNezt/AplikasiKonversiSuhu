5ec1f96c0893ff0ce18c3cd7fdbd1a8e6c6849a7
KoriQorry <korivond@gmail.com>
2024-11-10 16:13:06 +0700

    Membuat Template dan Method
    Menambahkan source code

--- src/AplikasiSuhuForm.java
+++ src/AplikasiSuhuForm.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -14,8 +17,20 @@ public class AplikasiSuhuForm extends javax.swing.JFrame {
      */
     public AplikasiSuhuForm() {
         initComponents();
+        Color col = new Color(194,218,249);
+        getContentPane().setBackground(col);
+        txtFahrenheit.setFocusable(true); 
+        txtReamur.setFocusable(true); 
+        txtKelvin.setFocusable(true); 
     }
 
+    private void clearFields() {
+        // Method Membersihkan Semua Fields
+        txtCelsius.setText("");
+        txtFahrenheit.setText("");
+        txtReamur.setText("");
+        txtKelvin.setText(""); 
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -25,21 +40,167 @@ public class AplikasiSuhuForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtCelsius = new javax.swing.JTextField();
+        txtFahrenheit = new javax.swing.JTextField();
+        txtReamur = new javax.swing.JTextField();
+        txtKelvin = new javax.swing.JTextField();
+        btnHitung = new javax.swing.JButton();
+        btnClear = new javax.swing.JButton();
+        btnQuit = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(194, 218, 249));
+
+        jLabel1.setFont(new java.awt.Font("Bahnschrift", 0, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Aplikasi Konversi Suhu");
+
+        jLabel2.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        jLabel2.setText("Celsius");
+
+        jLabel3.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        jLabel3.setText("Fahrenheit");
+
+        jLabel4.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        jLabel4.setText("Reamur");
+
+        jLabel5.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        jLabel5.setText("Kelvin");
+
+        txtCelsius.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        txtCelsius.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCelsiusActionPerformed(evt);
+            }
+        });
+
+        txtFahrenheit.setEditable(false);
+        txtFahrenheit.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        txtFahrenheit.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtFahrenheitFocusGained(evt);
+            }
+        });
+
+        txtReamur.setEditable(false);
+        txtReamur.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        txtReamur.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtReamurFocusGained(evt);
+            }
+        });
+
+        txtKelvin.setEditable(false);
+        txtKelvin.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        txtKelvin.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtKelvinFocusGained(evt);
+            }
+        });
+
+        btnHitung.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        btnHitung.setText("Hitung");
+
+        btnClear.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        btnClear.setText("Hapus");
+        btnClear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnClearActionPerformed(evt);
+            }
+        });
+
+        btnQuit.setFont(new java.awt.Font("Bahnschrift", 0, 14)); // NOI18N
+        btnQuit.setText("Keluar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(56, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnQuit)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txtCelsius)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(btnHitung)
+                                .addGap(18, 18, 18)
+                                .addComponent(btnClear))
+                            .addComponent(txtFahrenheit, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtReamur, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtKelvin, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(104, 104, 104))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(65, 65, 65))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addComponent(btnQuit)
+                .addGap(10, 10, 10)
+                .addComponent(jLabel1)
+                .addGap(26, 26, 26)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtCelsius, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHitung)
+                    .addComponent(btnClear))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtFahrenheit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtReamur, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtKelvin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addContainerGap(44, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearActionPerformed
+        clearFields();
+    }//GEN-LAST:event_btnClearActionPerformed
+
+    private void txtCelsiusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCelsiusActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCelsiusActionPerformed
+
+    private void txtFahrenheitFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtFahrenheitFocusGained
+        txtFahrenheit.selectAll();
+    }//GEN-LAST:event_txtFahrenheitFocusGained
+
+    private void txtReamurFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtReamurFocusGained
+        txtReamur.selectAll();
+    }//GEN-LAST:event_txtReamurFocusGained
+
+    private void txtKelvinFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtKelvinFocusGained
+        txtKelvin.selectAll();
+    }//GEN-LAST:event_txtKelvinFocusGained
 
     /**
      * @param args the command line arguments
@@ -77,5 +238,17 @@ public class AplikasiSuhuForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnHitung;
+    private javax.swing.JButton btnQuit;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField txtCelsius;
+    private javax.swing.JTextField txtFahrenheit;
+    private javax.swing.JTextField txtKelvin;
+    private javax.swing.JTextField txtReamur;
     // End of variables declaration//GEN-END:variables
 }
